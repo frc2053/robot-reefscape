7e727c65e284857f366724b5502a570f01d91650
Drew Williams <DrewW@iARx.com>
2025-04-02 15:50:49 -0400

    wierd timing thing

--- robot.py
+++ robot.py
@@ -8,71 +8,50 @@
 import commands2
 import typing
 
+import wpilib
+
 from robotcontainer import RobotContainer
+from phoenix6.signal_logger import SignalLogger
+from wpilib import DataLogManager, DriverStation
+from wpinet import WebServer
 
 
 class MyRobot(commands2.TimedCommandRobot):
-    """
-    Command v2 robots are encouraged to inherit from TimedCommandRobot, which
-    has an implementation of robotPeriodic which runs the scheduler for you
-    """
-
     autonomousCommand: typing.Optional[commands2.Command] = None
 
     def robotInit(self) -> None:
-        """
-        This function is run when the robot is first started up and should be used for any
-        initialization code.
-        """
+        SignalLogger.enable_auto_logging(True)
+        SignalLogger.start()
+        DataLogManager.start()
+        DriverStation.startDataLog(DataLogManager.getLog(), True)
+        WebServer.getInstance().start(5800, wpilib.getDeployDirectory())
 
-        # Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-        # autonomous chooser on the dashboard.
         self.container = RobotContainer()
 
     def robotPeriodic(self) -> None:
-        """This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-        that you want ran during disabled, autonomous, teleoperated and test.
-
-        This runs after the mode specific periodic functions, but before LiveWindow and
-        SmartDashboard integrated updating."""
-
-        # Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-        # commands, running already-scheduled commands, removing finished or interrupted commands,
-        # and running subsystem periodic() methods.  This must be called from the robot's periodic
-        # block in order for anything in the Command-based framework to work.
         commands2.CommandScheduler.getInstance().run()
 
     def disabledInit(self) -> None:
-        """This function is called once each time the robot enters Disabled mode."""
         pass
 
     def disabledPeriodic(self) -> None:
-        """This function is called periodically when disabled"""
         pass
 
     def autonomousInit(self) -> None:
-        """This autonomous runs the autonomous command selected by your RobotContainer class."""
         self.autonomousCommand = self.container.getAutonomousCommand()
 
         if self.autonomousCommand:
             self.autonomousCommand.schedule()
 
     def autonomousPeriodic(self) -> None:
-        """This function is called periodically during autonomous"""
         pass
 
     def teleopInit(self) -> None:
-        # This makes sure that the autonomous stops running when
-        # teleop starts running. If you want the autonomous to
-        # continue until interrupted by another command, remove
-        # this line or comment it out.
         if self.autonomousCommand:
             self.autonomousCommand.cancel()
 
     def teleopPeriodic(self) -> None:
-        """This function is called periodically during operator control"""
         pass
 
     def testInit(self) -> None:
-        # Cancels all running commands at the start of test mode
         commands2.CommandScheduler.getInstance().cancelAll()
--- robotcontainer.py
+++ robotcontainer.py
@@ -20,22 +20,10 @@ from wpimath.units import rotationsToRadians
 
 
 class RobotContainer:
-    """
-    This class is where the bulk of the robot should be declared. Since Command-based is a
-    "declarative" paradigm, very little robot logic should actually be handled in the :class:`.Robot`
-    periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
-    subsystems, commands, and button mappings) should be declared here.
-    """
-
     def __init__(self) -> None:
-        self._max_speed = (
-            TunerConstants.speed_at_12_volts
-        )  # speed_at_12_volts desired top speed
-        self._max_angular_rate = rotationsToRadians(
-            0.75
-        )  # 3/4 of a rotation per second max angular velocity
+        self._max_speed = TunerConstants.speed_at_12_volts
+        self._max_angular_rate = rotationsToRadians(1.0)
 
-        # Setting up bindings for necessary control of the swerve drive platform
         self._drive = (
             swerve.requests.FieldCentric()
             .with_deadband(self._max_speed * 0.1)
@@ -58,35 +46,22 @@ class RobotContainer:
 
         self.drivetrain = TunerConstants.create_drivetrain()
 
-        # Path follower
         self._auto_chooser = AutoBuilder.buildAutoChooser("Tests")
         SmartDashboard.putData("Auto Mode", self._auto_chooser)
 
-        # Configure the button bindings
         self.configureButtonBindings()
 
     def configureButtonBindings(self) -> None:
-        """
-        Use this method to define your button->command mappings. Buttons can be created by
-        instantiating a :GenericHID or one of its subclasses (Joystick or XboxController),
-        and then passing it to a JoystickButton.
-        """
-
-        # Note that X is defined as forward according to WPILib convention,
-        # and Y is defined as to the left according to WPILib convention.
         self.drivetrain.setDefaultCommand(
-            # Drivetrain will execute this command periodically
             self.drivetrain.apply_request(
                 lambda: (
                     self._drive.with_velocity_x(
                         -self._joystick.getLeftY() * self._max_speed
-                    )  # Drive forward with negative Y (forward)
-                    .with_velocity_y(
-                        -self._joystick.getLeftX() * self._max_speed
-                    )  # Drive left with negative X (left)
+                    )
+                    .with_velocity_y(-self._joystick.getLeftX() * self._max_speed)
                     .with_rotational_rate(
                         -self._joystick.getRightX() * self._max_angular_rate
-                    )  # Drive counterclockwise with negative X (left)
+                    )
                 )
             )
         )
@@ -111,8 +86,6 @@ class RobotContainer:
             )
         )
 
-        # Run SysId routines when holding back/start and X/Y.
-        # Note that each routine should be run exactly once in a single log.
         (self._joystick.back() & self._joystick.y()).whileTrue(
             self.drivetrain.sys_id_dynamic(SysIdRoutine.Direction.kForward)
         )
@@ -126,7 +99,6 @@ class RobotContainer:
             self.drivetrain.sys_id_quasistatic(SysIdRoutine.Direction.kReverse)
         )
 
-        # reset the field-centric heading on left bumper press
         self._joystick.leftBumper().onTrue(
             self.drivetrain.runOnce(lambda: self.drivetrain.seed_field_centric())
         )
@@ -136,8 +108,4 @@ class RobotContainer:
         )
 
     def getAutonomousCommand(self) -> commands2.Command:
-        """Use this to pass the autonomous command to the main {@link Robot} class.
-
-        :returns: the command to run in autonomous
-        """
         return self._auto_chooser.getSelected()
