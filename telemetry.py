b0d60889d6aa6af568f5e52f55d21fdcb07beec3
Drew Williams <DrewW@iARx.com>
2025-04-16 12:21:10 -0400

    added ascope layout

--- telemetry.py
+++ telemetry.py
@@ -1,6 +1,5 @@
 from ntcore import NetworkTableInstance
 from phoenix6 import SignalLogger, swerve, units
-from wpilib import Color, Color8Bit, Mechanism2d, MechanismLigament2d, SmartDashboard
 from wpimath.geometry import Pose2d
 from wpimath.kinematics import ChassisSpeeds, SwerveModulePosition, SwerveModuleState
 
@@ -48,44 +47,6 @@ class Telemetry:
         self._field_pub = self._table.getDoubleArrayTopic("robotPose").publish()
         self._field_type_pub = self._table.getStringTopic(".type").publish()
 
-        # Mechanisms to represent the swerve module states
-        self._module_mechanisms: list[Mechanism2d] = [
-            Mechanism2d(1, 1),
-            Mechanism2d(1, 1),
-            Mechanism2d(1, 1),
-            Mechanism2d(1, 1),
-        ]
-        # A direction and length changing ligament for speed representation
-        self._module_speeds: list[MechanismLigament2d] = [
-            self._module_mechanisms[0]
-            .getRoot("RootSpeed", 0.5, 0.5)
-            .appendLigament("Speed", 0.5, 0),
-            self._module_mechanisms[1]
-            .getRoot("RootSpeed", 0.5, 0.5)
-            .appendLigament("Speed", 0.5, 0),
-            self._module_mechanisms[2]
-            .getRoot("RootSpeed", 0.5, 0.5)
-            .appendLigament("Speed", 0.5, 0),
-            self._module_mechanisms[3]
-            .getRoot("RootSpeed", 0.5, 0.5)
-            .appendLigament("Speed", 0.5, 0),
-        ]
-        # A direction changing and length constant ligament for module direction
-        self._module_directions: list[MechanismLigament2d] = [
-            self._module_mechanisms[0]
-            .getRoot("RootDirection", 0.5, 0.5)
-            .appendLigament("Direction", 0.1, 0, 0, Color8Bit(Color.kWhite)),
-            self._module_mechanisms[1]
-            .getRoot("RootDirection", 0.5, 0.5)
-            .appendLigament("Direction", 0.1, 0, 0, Color8Bit(Color.kWhite)),
-            self._module_mechanisms[2]
-            .getRoot("RootDirection", 0.5, 0.5)
-            .appendLigament("Direction", 0.1, 0, 0, Color8Bit(Color.kWhite)),
-            self._module_mechanisms[3]
-            .getRoot("RootDirection", 0.5, 0.5)
-            .appendLigament("Direction", 0.1, 0, 0, Color8Bit(Color.kWhite)),
-        ]
-
     def telemeterize(self, state: swerve.SwerveDrivetrain.SwerveDriveState):
         """
         Accept the swerve drive state and telemeterize it to SmartDashboard and SignalLogger.
@@ -121,11 +82,3 @@ class Telemetry:
         # Telemeterize the pose to a Field2d
         self._field_type_pub.set("Field2d")
         self._field_pub.set(pose_array)
-
-        # Telemeterize the module states to a Mechanism2d
-        for i, module_state in enumerate(state.module_states):
-            self._module_speeds[i].setAngle(module_state.angle.degrees())
-            self._module_directions[i].setAngle(module_state.angle.degrees())
-            self._module_speeds[i].setLength(module_state.speed / (2 * self._max_speed))
-
-            SmartDashboard.putData(f"Module {i}", self._module_mechanisms[i])
