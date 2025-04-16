09644a2e159fd64ffeaba08f9c44c0462d0ce836
Andrew Lee <andrewlee7919@gmail.com>
2025-04-16 19:40:20 -0400

    ?

--- subsystems/Elevator.py
+++ subsystems/Elevator.py
@@ -1,34 +1,38 @@
 from constants import ElevatorConstants as consts
-from phoenix import StatusCodes
-from phoenix6 import StatusSignal
+from phoenix6 import StatusCode, controls
+from phoenix6.controls import VoltageOut
+from phoenix6 import BaseStatusSignal
 from phoenix6.configs import Configs
 from phoenix6.signals import SpnEnums
-from frc import DataLogManager, RobotBase, RobotController
-from frc.controller import ElevatorFeedforward
-from frc.trajectory import ExponentialProfile, TrapezoidProfile
-from frc2.command import CommandPtr, Commands
-from frc2.command.button import Trigger
-from str import GainTypes, Units
-from units import acceleration, angle, angular_velocity, current, length, time, velocity, voltage
-from units import math as units_math
-
+import math
+import wpilib #Maybe?
+from wpilib import DataLogManager, RobotBase, RobotController
+from wpimath.controller import ElevatorFeedforward, PIDController
+from wpimath.trajectory import ExponentialProfile, TrapezoidProfile
+from commands2 import CommandPtr, Commands
+from commands2.button import Trigger
+from str import GainTypes
 
 class Elevator:
     def __init__(self,display):
-        self.display = display
-        self.ConfigureMotors
+        self.display = display #First grap the display of the thingy
+        self.ConfigureMotors 
         self.ConfigureControlSignals
         self.OptimiveBusSignals
+        self._trapSetpoint = TrapezoidProfile.State
+        self._trapGoal = TrapezoidProfile.State
+        self._elevatorProfile = TrapezoidProfile(TrapezoidProfile.Constraints(3.048, 6.096))
+        self._elevatorVoltageSetter = VoltageOut(0)
+        self._elevatorPID = PIDController
 
-        self.frontMotor.SetPosition(0) #might have to do 0* length.turn
+        self.frontMotor.SetPosition(0) #might have to do 0* length.turn???  idk
         self.backMotor.SetPosition(0)
 
+        #finished the settings stuff
+
     
-    def __init__(Periodic):
-        status = ctre.StatusCode(BaseStatusSignal.WaitForAll)( #extremely questionable - needs to develop library first 
-            2.0/consts.Elevator.BUS_UPDATE_FREQ, self.frontPositionSig, self.frontVelocitySig,
-            self.FrontvoltagesSig, self.backPositionSig, self.backVelocitySig, self.backVoltageSig)
-        
+    def periodic_update(self):
+        status = BaseStatusSignal.wait_for_all(self.FrontvoltagesSig, self.backPositionSig, self.backVelocitySig, self.backVoltageSig)
 
         if not (status.IsOK()):
             DataLogManager.Log(f"Error updating elevator positions! Error was: {status.GetName()}")
@@ -38,7 +42,62 @@ class Elevator:
         self.currentHeight = self.GetHeight()
 
         #continued on line 59
-
-        next = self.trapProf.Calculate(20 * time.ms, self.trapSetpoint, self.trapGoal)
+        next = self._elevatorProfile.Calculate(0.02, self._trapSetpoint, self._trapGoal) #I have no idea what a trapezoid profile is so this is it for now lol
         #line 61? idk
+
+        currentKg = 120000
+        kP = 1
+        kI = 2
+        kD = 3 #originally currentGains.kP.value() 
+        kS = 12
+        vA = 10
+        kA = 0 #?
+        isCharacterizing = False
+
+        NUM_OF_STAGES = 2
+        PULLEY_DIAM = 1
+
+        ff = ElevatorFeedforward(kS, currentKg, vA, kA) #grabbing instance? idk if needed cause idk why the str import not working :/
+        def GetElevatorVel():
+            avgVel = ConvertEncVelToHeightVel(self.frontVelocitySig.GetValue() + self.backVelocitySig.GetValue()) / 2.0 * NUM_OF_STAGES
+            return avgVel
+
+        def ConvertEncVelToHeightVel(radialVel):
+            ret = (radialVel / math.radians(180)) * (PULLEY_DIAM / 2.0)
+            return ret
+        
+        ffToSend = 0
+        ffToSend = ff.Calculate(GetElevatorVel(), next.velocity)
+
+        elevatorPid = wpilib.PIDController(kP,kI, kD)
+        
+        pidOutput = elevatorPid.Calculate(self.currentHeight.value(), next.position.value())
+
+
+        if not (isCharacterizing):
+            self.frontMotor.SetControl(self._elevatorVoltageSetter.with_output(ffToSend + pidOutput).with_enable_foc(True))
+        else:
+            pass
+
+        height_tolerance = 0.5 #inches
+
+        isAtGoalHeight = False
+        isAtGoalHeigh = abs(self.trapGoal.position - self.currentHeight) < height_tolerance
+        
+        def set_elevator_height(new_height):
+            pass
+
+        self.display(set_elevator_height(self.currentHeight))
+
+        UpdateNTEntries()
+
+        trapSetpoint = next
+    
+    def UpdateNTEntries():
+        pass
+
+
+
+
+
 
